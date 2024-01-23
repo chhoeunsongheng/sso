80535a6cb469f80eb73d74aa9bc8044a556f0125
chhoeunsongheng <chhoeunsongheng@gmail.com>
2024-01-23 22:48:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {Amplify} from 'aws-amplify';
+import awsExports from "./aws-exports";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Auth } from "aws-amplify";
+
+// Configure Amplify in index file or root file
+Amplify.configure({
+  Auth: {
+    // region: awsExports.REGION,
+    // userPoolId: awsExports.USER_POOL_ID,
+    // userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+    Cognito: {
+      userPoolClientId: awsExports.USER_POOL_APP_CLIENT_ID,
+      userPoolId: awsExports.USER_POOL_ID,
+      loginWith: { // Optional
+        oauth: {
+          domain: 'https://wingmallpool.auth.ap-southeast-2.amazoncognito.com',
+          scopes: ['openid email phone profile aws.cognito.signin.user.admin '],
+          redirectSignIn: ['http://localhost:3000/','https://example.com/'],
+          redirectSignOut: ['http://localhost:3000/','https://example.com/'],
+          responseType: 'code',
+        },
+        username: 'true',
+        email: 'false', // Optional
+        phone: 'false', // Optional
+      }
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+            <div>
+              <p>Welcome {user.username}</p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+        )}
+        {/* <div>Hello</div> */}
+      </Authenticator>
   );
 }
 
---
+++ src/aws-exports.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    "REGION": "ap-southeast-2",
+    "USER_POOL_ID": "ap-southeast-2_dWzjnMmP9",
+    "USER_POOL_APP_CLIENT_ID": "2pghpg7ubvj8otn7aic6fnbbtt"
+}
